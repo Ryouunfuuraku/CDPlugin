c04ccbf54539277766ef9c2c4caac384096e7e85
Huang Yongyan <lingyunfengluo@gmail.com>
2019-03-05 21:40:51 +0800

    Remember Logout Pos
    Remember Logout Pos

--- ClassLibrary1/ClassLibrary1/Class1.cs
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -1,20 +1,15 @@
 ﻿using System;
 using System.IO;
-using System.Data;
 using Terraria;
 using Terraria.DataStructures;
 using TerrariaApi.Server;
-using TShockAPI.DB;
-using Terraria.Localization;
+using TShockAPI.Hooks;
 using TShockAPI;
-using Mono.Data.Sqlite;
-using MySql.Data.MySqlClient;
 namespace CDPlugin
 {
     [ApiVersion(2, 1)]
     public class CDPlugin : TerrariaPlugin
     {
-        private static IDbConnection db;
         public override string Author => "Ryouun";
         public override string Description => "Drop item when player die.";
         public override string Name => "CD";
@@ -31,7 +26,7 @@ namespace CDPlugin
         private const int MEDDR = 95;
         // 扩展栏掉落物品的概率
         private const int MEDR = 33;
-
+        public static TSPlayer[] Players = new TSPlayer[Main.maxPlayers];
         public CDPlugin(Main game) : base(game)
         {
 
@@ -39,22 +34,46 @@ namespace CDPlugin
         public override void Initialize()
         {
             ServerApi.Hooks.NetGetData.Register(this, NetHooks_GetData);
-            ServerApi.Hooks.GamePostInitialize.Register(this, OnDBInitialize);
-            
+            ServerApi.Hooks.GameInitialize.Register(this, OnInit);
+            ServerApi.Hooks.ServerJoin.Register(this, OnLogin);
+            ServerApi.Hooks.ServerLeave.Register(this, OnLogout);
         }
-
 
         protected override void Dispose(bool disposing)
         {
             if (disposing)
             {
                 ServerApi.Hooks.NetGetData.Deregister(this, NetHooks_GetData);
+                ServerApi.Hooks.GameInitialize.Deregister(this, OnInit);
+                ServerApi.Hooks.ServerJoin.Deregister(this, OnLogin);
+                ServerApi.Hooks.ServerLeave.Deregister(this, OnLogout);
             }
             base.Dispose(disposing);
         }
 
+        private void OnInit(EventArgs args)
+        {
+            DB.Connect();
+        }
+
+        private void OnLogin(JoinEventArgs args)
+        {
+            float x;
+            float y;
+            PlayerPos  pp = DB.GetPos(Players[args.Who]);
+            x = pp.x;
+            y = pp.y;
+            Players[args.Who].Teleport(x,y);
+        }
+
+        private void OnLogout(LeaveEventArgs args)
+        {
+            DB.SetPos(Players[args.Who]);
+        }
+        
         private void NetHooks_GetData(GetDataEventArgs args)
         {
+
             if (args.MsgID == PacketTypes.PlayerDeathV2)
             {
                 args.Msg.reader.BaseStream.Position = args.Index;
@@ -71,15 +90,42 @@ namespace CDPlugin
                     {
                         return;
                     }
-                    DDrop(Main.player[playerId], Main.player[playerId].inventory, 0, InventoryDDR, InventoryDR);
-                    DDrop(Main.player[playerId], Main.player[playerId].armor, 59, ArmDDR, ArmDR);
-                    DDrop(Main.player[playerId], Main.player[playerId].miscEquips, 89, MEDDR, MEDR);
+                    InventoryDrop(Main.player[playerId], Main.player[playerId].inventory, 0, InventoryDDR, InventoryDR);
+                    ArmDrop(Main.player[playerId], Main.player[playerId].armor, 59, ArmDDR, ArmDR);
+                    MiscItemDrop(Main.player[playerId], Main.player[playerId].miscEquips, 89, MEDDR, MEDR);
                     
                 }
             }
         }
 
-        public void DDrop(Player p, Item[] inv, int os, int dontdestroyrate, int droprate)
+        public void InventoryDrop(Player p, Item[] inv, int os, int dontdestroyrate, int droprate)
+        {
+            for (var i = 0; i < inv.Length; i++)
+            {
+                if (Main.rand.Next(100) <= droprate || i < 10)
+                {
+                    if (inv[i].Name != "")
+                    {
+                        DropItem(p, inv[i], os + i, dontdestroyrate);
+                    }
+                }
+            }
+        }
+        public void ArmDrop(Player p, Item[] inv, int os, int dontdestroyrate, int droprate)
+        {
+
+            for (var i = 0; i < inv.Length; i++)
+            {
+                if (Main.rand.Next(100) <= droprate && (i < 3 || (i > 9 && i < 13)))
+                {
+                    if (inv[i].Name != "")
+                    {
+                        DropItem(p, inv[i], os + i, dontdestroyrate);
+                    }
+                }
+            }
+        }
+        public void MiscItemDrop(Player p, Item[] inv, int os, int dontdestroyrate, int droprate)
         {
             for (var i = 0; i < inv.Length; i++)
             {
@@ -95,11 +141,11 @@ namespace CDPlugin
 
         void DropItem(Player p, Item i, int itemID, int ddrate)
         {
-            if(Main.rand.Next(100) <= ddrate)
+            if (Main.rand.Next(100) <= ddrate)
             {
                 if (i.stack > 1)
                 {
-                    var id = Item.NewItem(p.position, i.width, i.height, i.type, (int)Math.Round(i.stack*Main.rand.Next(100)/100.0), true, i.prefix, true);
+                    var id = Item.NewItem(p.position, i.width, i.height, i.type, (int)Math.Round(i.stack * Main.rand.Next(100) / 100.0), true, i.prefix, true);
                 }
                 else
                 {
@@ -110,40 +156,20 @@ namespace CDPlugin
             i.active = false;
             NetMessage.SendData(5, -1, -1, null, p.whoAmI, itemID, i.prefix);
         }
-
-        private void OnDBInitialize(EventArgs args)
+        
+        public enum ItemSlot
         {
-            switch (TShock.Config.StorageType.ToLower())
-            {
-                case "mysql":
-                    string[] dbHost = TShock.Config.MySqlHost.Split(':');
-                    db = new MySqlConnection()
-                    {
-                        ConnectionString = string.Format("Server={0}; Port={1}; Database={2}; Uid={3}; Pwd={4};",
-                            dbHost[0],
-                            dbHost.Length == 1 ? "3306" : dbHost[1],
-                            TShock.Config.MySqlDbName,
-                            TShock.Config.MySqlUsername,
-                            TShock.Config.MySqlPassword)
-
-                    };
-                    break;
-
-                case "sqlite":
-                    string sql = Path.Combine(TShock.SavePath, "CDRPG.sqlite");
-                    db = new SqliteConnection(string.Format("uri=file://{0},Version=3", sql));
-                    break;
-
-            }
-
-            SqlTableCreator sqlcreator = new SqlTableCreator(db, db.GetSqlType() == SqlType.Sqlite ? (IQueryBuilder)new SqliteQueryCreator() : new MysqlQueryCreator());
-
-            sqlcreator.EnsureTableStructure(new SqlTable("CDRPG",
-                new SqlColumn("ID", MySqlDbType.Int32) { Primary = true, Unique = true, Length = 4 },
-                new SqlColumn("UID", MySqlDbType.Int32) { Length = 1 },
-                new SqlColumn("Name", MySqlDbType.Text) { Length = 15 },
-                new SqlColumn("Date", MySqlDbType.Text) { Length = 30 },
-                new SqlColumn("Quote", MySqlDbType.Text) { Length = 100 }));
+            InvRow1Slot1, InvRow1Slot2, InvRow1Slot3, InvRow1Slot4, InvRow1Slot5, InvRow1Slot6, InvRow1Slot7, InvRow1Slot8, InvRow1Slot9, InvRow1Slot10,
+            InvRow2Slot1, InvRow2Slot2, InvRow2Slot3, InvRow2Slot4, InvRow2Slot5, InvRow2Slot6, InvRow2Slot7, InvRow2Slot8, InvRow2Slot9, InvRow2Slot10,
+            InvRow3Slot1, InvRow3Slot2, InvRow3Slot3, InvRow3Slot4, InvRow3Slot5, InvRow3Slot6, InvRow3Slot7, InvRow3Slot8, InvRow3Slot9, InvRow3Slot10,
+            InvRow4Slot1, InvRow4Slot2, InvRow4Slot3, InvRow4Slot4, InvRow4Slot5, InvRow4Slot6, InvRow4Slot7, InvRow4Slot8, InvRow4Slot9, InvRow4Slot10,
+            InvRow5Slot1, InvRow5Slot2, InvRow5Slot3, InvRow5Slot4, InvRow5Slot5, InvRow5Slot6, InvRow5Slot7, InvRow5Slot8, InvRow5Slot9, InvRow5Slot10,
+            CoinSlot1, CoinSlot2, CoinSlot3, CoinSlot4, AmmoSlot1, AmmoSlot2, AmmoSlot3, AmmoSlot4, HandSlot,
+            ArmorHeadSlot, ArmorBodySlot, ArmorLeggingsSlot, AccessorySlot1, AccessorySlot2, AccessorySlot3, AccessorySlot4, AccessorySlot5, AccessorySlot6, UnknownSlot1,
+            VanityHeadSlot, VanityBodySlot, VanityLeggingsSlot, SocialAccessorySlot1, SocialAccessorySlot2, SocialAccessorySlot3, SocialAccessorySlot4, SocialAccessorySlot5, SocialAccessorySlot6, UnknownSlot2,
+            DyeHeadSlot, DyeBodySlot, DyeLeggingsSlot, DyeAccessorySlot1, DyeAccessorySlot2, DyeAccessorySlot3, DyeAccessorySlot4, DyeAccessorySlot5, DyeAccessorySlot6, Unknown3,
+            EquipmentSlot1, EquipmentSlot2, EquipmentSlot3, EquipmentSlot4, EquipmentSlot5,
+            DyeEquipmentSlot1, DyeEquipmentSlot2, DyeEquipmentSlot3, DyeEquipmentSlot4, DyeEquipmentSlot5
         }
     }
 }
---
+++ ClassLibrary1/ClassLibrary1/DB.cs
@@ -0,0 +1,134 @@
+﻿using Mono.Data.Sqlite;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.IO;
+using TShockAPI;
+using TShockAPI.DB;
+
+namespace CDPlugin
+{
+    public static class DB
+    {
+        private static IDbConnection db;
+
+        public static void Connect()
+        {
+            switch (TShock.Config.StorageType.ToLower())
+            {
+                case "mysql":
+                    string[] dbHost = TShock.Config.MySqlHost.Split(':');
+                    db = new MySqlConnection()
+                    {
+                        ConnectionString = string.Format("Server={0}; Port={1}; Database={2}; Uid={3}; Pwd={4};",
+                            dbHost[0],
+                            dbHost.Length == 1 ? "3306" : dbHost[1],
+                            TShock.Config.MySqlDbName,
+                            TShock.Config.MySqlUsername,
+                            TShock.Config.MySqlPassword)
+
+                    };
+                    break;
+
+                case "sqlite":
+                    string sql = Path.Combine(TShock.SavePath, "CDPlugin.sqlite");
+                    db = new SqliteConnection(string.Format("uri=file://{0},Version=3", sql));
+                    break;
+
+            }
+
+            SqlTableCreator sqlcreator = new SqlTableCreator(db, db.GetSqlType() == SqlType.Sqlite ? (IQueryBuilder)new SqliteQueryCreator() : new MysqlQueryCreator());
+
+            sqlcreator.EnsureTableStructure(new SqlTable("cdplugin",
+                new SqlColumn("userid", MySqlDbType.Int32) { Primary = true, Unique = true, Length = 6 },
+                new SqlColumn("x", MySqlDbType.Float) { },
+                new SqlColumn("y", MySqlDbType.Float) { }));
+        }
+
+        public static void AddPos(PlayerPos info)
+        {
+            string query = $"INSERT INTO `cdplugin` (`userid`, `x`, `y`) VALUES ({info.userid}, '{info.x}', '{info.y}');";
+            int result = db.Query(query);
+            if (result != 1)
+            {
+                TShock.Log.ConsoleError("Error adding entry to database for user: " + info.userid);
+            }
+        }
+
+        public static void UpdatePos(PlayerPos info)
+        {
+            string query = $"UPDATE `cdplugin` SET `x` = '{info.x}', `y` = '{info.y}' WHERE `userid` = {info.userid};";
+            int result = db.Query(query);
+            if (result != 1)
+            {
+                TShock.Log.ConsoleError("Error updating entry in database for user: " + info.userid);
+            }
+        }
+
+        public static void DeletePos(int userid)
+        {
+            string query = $"DELETE FROM `cdplugin` WHERE `userid` = {userid}";
+            int result = db.Query(query);
+            if (result != 1)
+            {
+                TShock.Log.ConsoleError("Error deleting entry in database for user: " + userid);
+            }
+        }
+
+        public static PlayerPos GetPos(TSPlayer plr)
+        {
+            PlayerPos retPos = new PlayerPos(plr, false);
+
+            string query = $"SELECT * FROM `cdplugin` WHERE `userid` = {plr.User.ID};";
+            using (var reader = db.QueryReader(query))
+            {
+                if (reader.Read())
+                {
+                    retPos.indatabase = true;
+                    retPos.x = reader.Get<float>("x");
+                    retPos.y = reader.Get<float>("y");
+                }
+                else
+                {
+                    retPos.indatabase = false;
+                    retPos.x = plr.X;
+                    retPos.y = plr.Y;
+                    AddPos(retPos);
+                }
+            }
+            return retPos;
+        }
+
+
+        public static void SetPos(TSPlayer plr)
+        {
+            //Using null to signify that it was not in database
+            PlayerPos newInfo = new PlayerPos(plr, false);
+
+            string query = $"SELECT * FROM `cdplugin` WHERE `userid` = {plr.User.ID};";
+            using (var reader = db.QueryReader(query))
+            {
+                if (reader.Read())
+                {
+                    newInfo.indatabase = true;
+                    newInfo.x = reader.Get<float>("x");
+                    newInfo.y = reader.Get<float>("y");
+                }
+            }
+        }
+    }
+
+    public class PlayerPos
+    {
+        public int userid;
+        public float x;
+        public float y;
+        public bool indatabase;
+        public PlayerPos(TSPlayer plr, bool _indatabase)
+        {
+            userid = plr.User.ID;
+            x = plr.X;
+            y = plr.Y;
+            indatabase = _indatabase;
+        }
+    }
+}
